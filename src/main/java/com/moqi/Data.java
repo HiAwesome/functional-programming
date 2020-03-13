54b4401129b631e07bce12add3d17ce868a392ed
moqi <moqimoqidea@gmail.com>
2020-03-13 22:37:59 +0800

    Add A0201TheCompanyProcess.java

---
+++ src/main/java/com/moqi/Data.java
@@ -0,0 +1,18 @@
+package com.moqi;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author moqi
+ * On 3/13/20 22:36
+ */
+
+public class Data {
+
+    public static final List<String> NAME_LIST = Collections.unmodifiableList(
+            Arrays.asList("tom", "jack", "smith")
+    );
+
+}
---
+++ src/main/java/com/moqi/java/a02/A0201TheCompanyProcess.java
@@ -0,0 +1,40 @@
+package com.moqi.java.a02;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+import static com.moqi.Data.NAME_LIST;
+
+/**
+ * 典型的公司业务处理例子（Java 实现）
+ *
+ * @author moqi
+ * On 3/13/20 22:30
+ */
+@Slf4j
+public class A0201TheCompanyProcess {
+
+
+    public static void main(String[] args) {
+        String names = new A0201TheCompanyProcess().cleanNames(NAME_LIST);
+        log.info("names:{}", names);
+    }
+
+    public String cleanNames(List<String> listOfNames) {
+        StringBuilder result = new StringBuilder();
+
+        for (String listOfName : listOfNames) {
+            if (listOfName.length() > 1) {
+                result.append(capitalizeString(listOfName)).append(",");
+            }
+        }
+
+        return result.substring(0, result.length() - 1);
+    }
+
+    public String capitalizeString(String s) {
+        return s.substring(0, 1).toUpperCase() + s.substring(1);
+    }
+
+}
