47ab60a8c270e3d85ef1ba14d83e288c01b7e509
moqi <moqimoqidea@gmail.com>
2020-03-13 21:47:12 +0800

    Add A0101Words.java

---
+++ src/main/java/com/moqi/a01/A0101Words.java
@@ -0,0 +1,60 @@
+package com.moqi.a01;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author moqi
+ * On 3/13/20 21:40
+ */
+@Slf4j
+public class A0101Words {
+
+    private final static Set<String> NON_WORDS = new HashSet<String>() {{
+        add("the");
+        add("and");
+        add("of");
+        add("to");
+        add("a");
+        add("i");
+        add("it");
+        add("in");
+        add("or");
+        add("is");
+        add("d");
+        add("s");
+        add("as");
+        add("so");
+        add("but");
+        add("be");
+    }};
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = wordFreq("hello hello world world world");
+        log.info("map:{}", map);
+    }
+
+    @SuppressWarnings({"SameParameterValue", "Java8MapApi"})
+    private static Map<String, Integer> wordFreq(String words) {
+        TreeMap<String, Integer> wordMap = new TreeMap<>();
+        Matcher m = Pattern.compile("\\w+").matcher(words);
+        while (m.find()) {
+            String word = m.group().toLowerCase();
+            if (!NON_WORDS.contains(word)) {
+                if (wordMap.get(word) == null) {
+                    wordMap.put(word, 1);
+                } else {
+                    wordMap.put(word, wordMap.get(word) + 1);
+                }
+            }
+        }
+        return wordMap;
+    }
+
+}
