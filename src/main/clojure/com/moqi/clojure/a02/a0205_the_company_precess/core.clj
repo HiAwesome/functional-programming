83ef58a00c948d5a45b6f5af32498b4a4cf17474
moqi <moqimoqidea@gmail.com>
2020-03-14 20:24:29 +0800

    Update core.clj

--- src/main/clojure/com/moqi/clojure/a02/a0205_the_company_precess/core.clj
+++ src/main/clojure/com/moqi/clojure/a02/a0205_the_company_precess/core.clj
@@ -1,12 +1,15 @@
 (ns com.moqi.clojure.a02.a0205_the_company_precess.core
   (:require [clojure.string :as s]))
 
+; 通过 Clojure 实现 Process 过程
 ; BEGIN clj_process
 (defn process [list-of-employees]
   (reduce str (interpose ","
                          (map s/capitalize (filter #(< 1 (count %)) list-of-employees)))))
 ; END clj_process
 
+
+; 通过 Thread-last 宏改善代码的可读性
 ; BEGIN clj_process_thread
 (defn process2 [list-of-employees]
   (->> list-of-employees
