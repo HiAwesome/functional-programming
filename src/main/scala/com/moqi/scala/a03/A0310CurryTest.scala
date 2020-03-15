f99af960041e9662a798d34ff092d9e2f7c801cd
moqi <moqimoqidea@gmail.com>
2020-03-15 11:19:19 +0800

    Update A0310CurryTest.scala

--- src/main/scala/com/moqi/scala/a03/A0310CurryTest.scala
+++ src/main/scala/com/moqi/scala/a03/A0310CurryTest.scala
@@ -8,6 +8,7 @@ object A0310CurryTest extends App {
     else if (p(xs.head)) xs.head :: filter(xs.tail, p)
     else filter(xs.tail, p)
 
+  // 递归式的函数筛选，Scala 实现
   def dividesBy(n: Int)(x: Int) = ((x % n) == 0) // <1>
 
   val nums = List(1, 2, 3, 4, 5, 6, 7, 8)
